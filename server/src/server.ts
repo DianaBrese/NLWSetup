5dab557e1626db7c31ec5afa825b43f4adaa274f
Diana <dianabrese15@gmail.com>
2023-04-11 11:12:02 -0300

    Configurações iniciais do backend, front end e mobile

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+
+import { AppRoutes } from "./lib/routes";
+
+const app = Fastify();
+
+app.register(cors);
+app.register(AppRoutes)
+
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running");
+  });
---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+//import { Habit } from "./assets/components/Habit"
+
+import { Header } from "./components/Header";
+import "./App.css";
+import { SummaryTable } from "./components/SummaryTable";
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex-col gap-16">
+      <Header />
+      <SummaryTable />
+      </div>
+    </div>
+  );
+}
