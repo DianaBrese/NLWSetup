6ab102de2cab85dfe1ea2779cff9048f02a12b86
Diana <dianabrese15@gmail.com>
2023-04-12 11:05:04 -0300

    Criação de rota de resumo de dias

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -10,7 +10,7 @@ export async function AppRoutes(app: FastifyInstance) {
       weekDays: z.array(z.number().min(0).max(6)),
     });
 
-    console.log(createHabitBody)
+    console.log(createHabitBody);
     const { title, weekDays } = createHabitBody.parse(request.body);
 
     const today = dayjs().startOf("day").toDate();
@@ -124,5 +124,31 @@ export async function AppRoutes(app: FastifyInstance) {
         },
       });
     }
+  });
+
+  app.get("/summary", async () => {
+    const summary = await prisma.$queryRaw`
+    SELECT 
+      D.id, 
+      D.date,
+      (
+        SELECT 
+         cast(COUNT(*) as float) 
+        FROM day_habits DH
+        WHERE DH.day_id = D.id  
+      ) as completed,
+      (
+        SELECT  
+          cast(COUNT(*) as float) 
+        FROM habit_week_days HWD
+        JOIN habits H
+          ON H.id = HWD.habit_id
+        WHERE 
+          HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+          AND H.created_at <= D.date
+      ) as amount
+    FROM days D
+    `;
+    return summary;
   });
 }
