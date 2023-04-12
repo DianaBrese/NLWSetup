daef19e4449d5ba9ba748fb3b9c80e6d0e436f9e
Diana <dianabrese15@gmail.com>
2023-04-12 10:25:34 -0300

    Criação de rota para acrescentar hábitos

--- server/src/lib/routes.ts
+++ server/src/lib/routes.ts
@@ -9,6 +9,8 @@ export async function AppRoutes(app: FastifyInstance) {
       title: z.string(),
       weekDays: z.array(z.number().min(0).max(6)),
     });
+
+    console.log(createHabitBody)
     const { title, weekDays } = createHabitBody.parse(request.body);
 
     const today = dayjs().startOf("day").toDate();
@@ -54,7 +56,7 @@ export async function AppRoutes(app: FastifyInstance) {
       },
     });
 
-    const day = await prisma.day.findUnique({
+    const day = await prisma.day.findFirst({
       where: {
         date: parsedDate.toDate(),
       },
@@ -63,13 +65,64 @@ export async function AppRoutes(app: FastifyInstance) {
       },
     });
 
-    const completedHabits = day?.dayHabits.map(dayHabit => {
-        return dayHabit.habit_id
-    })
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
 
     return {
       possibleHabits,
-      completedHabits
+      completedHabits,
     };
+  });
+
+  //completar / não completar um hábito
+  app.patch("/habits/:id/toggle", async (request) => {
+    //route param => parâmetro de identificação
+
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
   });
 }
